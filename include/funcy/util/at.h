8d8e2af8b86031d2097bae06928ddf67ccab2f95
Lars Lubkoll <lars.lubkoll@posteo.de>
2020-07-04 23:58:47 +0200

    Move function at to namespace linalg

--- include/funcy/util/at.h
+++ include/funcy/util/at.h
@@ -5,50 +5,56 @@
 
 namespace funcy
 {
+    namespace linalg
+    {
+
 // suppress warnings coming from the __attribute__((always_inline))
 #if defined( __GNUC__ ) || defined( __GNUG__ )
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wattributes"
 #endif
 
-    /// Access matrix entry \f$A_{ij}\f via square brackets$.
-    template < Matrix M, class Index >
-    FUNCY_ALWAYS_INLINE decltype( auto ) at( M&& A, Index i,
-                                             Index j ) requires requires( std::decay_t< M > m )
-    {
-        { m[ 0 ][ 0 ] };
-    }
-    {
-        return A[ i ][ j ];
-    }
+        /// Access matrix entry \f$A_{ij}\f via square brackets$.
+        template < Matrix M, class Index >
+        FUNCY_ALWAYS_INLINE decltype( auto ) at( M&& A, Index i,
+                                                 Index j ) requires requires( std::decay_t< M > m )
+        {
+            { m[ 0 ][ 0 ] };
+        }
+        {
+            return A[ i ][ j ];
+        }
 
-    /// Access matrix entry \f$A_{ij}\f via square brackets$.
-    template < Matrix M, class Index >
-    FUNCY_ALWAYS_INLINE decltype( auto ) at( M&& A, Index i,
-                                             Index j ) requires requires( std::decay_t< M > m )
-    {
-        { m( 0, 0 ) };
-    }
-    {
-        return A( i, j );
-    }
+        /// Access matrix entry \f$A_{ij}\f via square brackets$.
+        template < Matrix M, class Index >
+        FUNCY_ALWAYS_INLINE decltype( auto ) at( M&& A, Index i,
+                                                 Index j ) requires requires( std::decay_t< M > m )
+        {
+            { m( 0, 0 ) };
+        }
+        {
+            return A( i, j );
+        }
 
-    /// Access vector entry \f$\v_if$.
-    template < Vector V, class Index >
-    FUNCY_ALWAYS_INLINE decltype( auto )
-    at( V&& v, Index i ) requires( !Matrix< V > && requires( std::decay_t< V > v ) { { v[ 0 ] }; } )
-    {
-        return v[ i ];
-    }
+        /// Access vector entry \f$\v_if$.
+        template < Vector V, class Index >
+        FUNCY_ALWAYS_INLINE decltype( auto )
+        at( V&& v,
+            Index i ) requires( !Matrix< V > && requires( std::decay_t< V > v ) { { v[ 0 ] }; } )
+        {
+            return v[ i ];
+        }
 
-    /// Access vector entry \f$\v_if$.
-    template < Vector V, class Index >
-    FUNCY_ALWAYS_INLINE decltype( auto )
-    at( V&& v, Index i ) requires( !Matrix< V > && requires( std::decay_t< V > v ) { { v( 0 ) }; } )
-    {
-        return v( i );
-    }
-} // namespace funcy
+        /// Access vector entry \f$\v_if$.
+        template < Vector V, class Index >
+        FUNCY_ALWAYS_INLINE decltype( auto )
+        at( V&& v,
+            Index i ) requires( !Matrix< V > && requires( std::decay_t< V > v ) { { v( 0 ) }; } )
+        {
+            return v( i );
+        }
 #if defined( __GNUC__ ) || defined( __GNUG__ )
 #pragma GCC diagnostic pop
 #endif
+    } // namespace linalg
+} // namespace funcy
