cae47e6c10241226dc2b31509c0bdaf4ee33dde6
Lars Lubkoll <lars.lubkoll@posteo.de>
2020-07-05 00:22:11 +0200

    Remove redundant abstraction

--- include/funcy/util/compute_conditional.h
+++ include/funcy/util/compute_conditional.h
@@ -8,89 +8,63 @@
 
 namespace funcy
 {
-    /// @cond
-    namespace detail
+    template < class X, class Y, bool = X::present, bool = Y::present >
+    struct ComputeConditional
     {
-        template < class X, class Y, bool = X::present, bool = Y::present >
-        struct ComputeConditionalImpl
-        {
-            static constexpr bool present = false;
-            ComputeConditionalImpl( const X&, const Y&, bool )
-            {
-            }
-        };
-
-        template < class X, class Y >
-        struct ComputeConditionalImpl< X, Y, true, true >
-        {
-            static constexpr bool present = true;
-
-            ComputeConditionalImpl( const X& x, const Y& y, bool choose_x )
-                : value( choose_x ? x() : y() )
-            {
-            }
-
-            decltype( auto ) operator()() const
-            {
-                return value;
-            }
-
-            remove_reference_t< decltype( std::declval< X >()() ) > value;
-        };
-
-        template < class X, class Y >
-        struct ComputeConditionalImpl< X, Y, true, false >
-        {
-            static constexpr bool present = true;
-
-            ComputeConditionalImpl( const X& x, const Y&, bool choose_x )
-                : value( choose_x ? x() : 0.0 )
-            {
-            }
-
-            decltype( auto ) operator()() const
-            {
-                return value;
-            }
-
-            decltype( std::declval< X >()() ) value;
-        };
-
-        template < class X, class Y >
-        struct ComputeConditionalImpl< X, Y, false, true >
-        {
-            static constexpr bool present = true;
-
-            ComputeConditionalImpl( const X&, const Y& y, bool choose_x )
-                : value( choose_x ? 0.0 : y() )
-            {
-            }
-
-            decltype( auto ) operator()() const
-            {
-                return value;
-            }
-
-            decltype( std::declval< Y >()() ) value;
-        };
-    } // namespace detail
-
-    template < class... >
-    struct ComputeConditional;
-
-    template < class X, class Y >
-    struct ComputeConditional< X, Y > : public detail::ComputeConditionalImpl< X, Y >
-    {
-        ComputeConditional( const X& x, const Y& y, bool choose_x )
-            : detail::ComputeConditionalImpl< X, Y, X::present, Y::present >( x, y, choose_x )
-        {
-        }
-
-        template < class F, class G, class... Args >
-        ComputeConditional( F const& f, G const& g, bool choose_x, Args&&... args )
-            : ComputeConditional( X( f, args... ), Y( g, args... ), choose_x )
+        static constexpr bool present = false;
+        ComputeConditional( const X&, const Y&, bool )
         {
         }
     };
-    /// @endcond
+
+    template < class X, class Y >
+    struct ComputeConditional< X, Y, true, true >
+    {
+        static constexpr bool present = true;
+
+        ComputeConditional( const X& x, const Y& y, bool choose_x ) : value( choose_x ? x() : y() )
+        {
+        }
+
+        decltype( auto ) operator()() const
+        {
+            return value;
+        }
+
+        remove_reference_t< decltype( std::declval< X >()() ) > value;
+    };
+
+    template < class X, class Y >
+    struct ComputeConditional< X, Y, true, false >
+    {
+        static constexpr bool present = true;
+
+        ComputeConditional( const X& x, const Y&, bool choose_x ) : value( choose_x ? x() : 0.0 )
+        {
+        }
+
+        decltype( auto ) operator()() const
+        {
+            return value;
+        }
+
+        decltype( std::declval< X >()() ) value;
+    };
+
+    template < class X, class Y >
+    struct ComputeConditional< X, Y, false, true >
+    {
+        static constexpr bool present = true;
+
+        ComputeConditional( const X&, const Y& y, bool choose_x ) : value( choose_x ? 0.0 : y() )
+        {
+        }
+
+        decltype( auto ) operator()() const
+        {
+            return value;
+        }
+
+        decltype( std::declval< Y >()() ) value;
+    };
 } // namespace funcy
