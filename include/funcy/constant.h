b6006f6aed4c94b3489099307448ff603e2ea365
Lars Lubkoll <lars.lubkoll@posteo.de>
2020-07-05 00:17:26 +0200

    Remove chainer from constant and add forwarding

--- include/funcy/constant.h
+++ include/funcy/constant.h
@@ -1,22 +1,29 @@
 #pragma once
 
-#include <funcy/util/chainer.h>
+#include <type_traits>
+#include <utility>
 
 namespace funcy
 {
     /// Wrap a constant.
     template < class Type >
-    struct Constant : Chainer< Constant< Type > >
+    struct Constant
     {
         constexpr Constant() = default;
 
-        /// Construct constant from copy.
-        constexpr Constant( Type t_ ) : t( t_ )
+        constexpr Constant( Type&& t_ ) noexcept( std::is_nothrow_move_constructible_v< Type > )
+            : t( std::move( t_ ) )
+        {
+        }
+
+        constexpr Constant( const Type& t_ ) noexcept(
+            std::is_nothrow_copy_constructible_v< Type > )
+            : t( t_ )
         {
         }
 
         /// Function value.
-        constexpr const Type& d0() const noexcept
+        constexpr const Type& operator()() const noexcept
         {
             return t;
         }
@@ -31,7 +38,7 @@ namespace funcy
      * This admits to use variable constant arguments, i.e. parameters that we want to study.
      */
     template < class Arg >
-    auto const_ref( const Arg& x )
+    auto const_ref( const Arg& x ) noexcept( std::is_nothrow_copy_constructible_v< Arg > )
     {
         return Constant< const Arg& >( x );
     }
@@ -41,8 +48,10 @@ namespace funcy
      * \return Constant<Arg>(x)
      */
     template < class Arg >
-    constexpr auto constant( Arg x )
+    constexpr auto constant( Arg&& x ) noexcept(
+        (std::is_rvalue_reference_v< Arg > && std::is_nothrow_move_constructible_v< Arg >) ||
+        (std::is_lvalue_reference_v< Arg > && std::is_nothrow_copy_constructible_v< Arg >))
     {
-        return Constant< Arg >( std::move( x ) );
+        return Constant( std::forward< Arg >( x ) );
     }
 } // namespace funcy
