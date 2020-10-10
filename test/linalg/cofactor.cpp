91ea856b9049e4a6ac066a1c61ca74d90d352c0d
Lars Lubkoll <lars.lubkoll@posteo.de>
2020-10-10 23:21:11 +0200

    Add tests for cofactors of runtime matrices

--- test/linalg/cofactor.cpp
+++ test/linalg/cofactor.cpp
@@ -1,12 +1,13 @@
-#include <Eigen/Dense>
-#include <gtest/gtest.h>
-
-#include <gmock/gmock.h>
-
-#include <tuple>
 
 #define FUNCY_ENABLE_EXCEPTIONS
 #include <funcy/linalg.h>
+
+#include <Eigen/Dense>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <tuple>
 
 using testing::DoubleEq;
 
@@ -14,8 +15,47 @@ namespace
 {
     using M2d = Eigen::Matrix< double, 2, 2 >;
     using M3d = Eigen::Matrix< double, 3, 3 >;
+    using MXd = Eigen::MatrixXd;
     using funcy::linalg::compute_cofactor;
     using funcy::linalg::compute_cofactor_directional_derivative;
+
+    auto get_first_matrix_2d()
+    {
+        M2d m;
+        m( 0, 0 ) = 1;
+        m( 0, 1 ) = 2;
+        m( 1, 0 ) = 3;
+        m( 1, 1 ) = 4;
+        return m;
+    }
+
+    auto get_second_matrix_2d()
+    {
+        M2d m;
+        m.fill( 0 );
+        m( 0, 0 ) = 1;
+        m( 1, 1 ) = 1;
+        return m;
+    }
+
+    auto get_first_matrix_x2d()
+    {
+        MXd m( 2, 2 );
+        m( 0, 0 ) = 1;
+        m( 0, 1 ) = 2;
+        m( 1, 0 ) = 3;
+        m( 1, 1 ) = 4;
+        return m;
+    }
+
+    auto get_second_matrix_x2d()
+    {
+        MXd m( 2, 2 );
+        m.fill( 0 );
+        m( 0, 0 ) = 1;
+        m( 1, 1 ) = 1;
+        return m;
+    }
 
     auto get_first_matrix_3d()
     {
@@ -43,28 +83,40 @@ namespace
         return m;
     }
 
-    auto get_first_matrix_2d()
+    auto get_first_matrix_x3d()
     {
-        M2d m;
+        MXd m( 3, 3 );
         m.fill( 0 );
-        m( 0, 0 ) = 1;
-        m( 0, 1 ) = 2;
+        m( 0, 0 ) = 0;
+        m( 0, 1 ) = 1;
+        m( 0, 2 ) = 2;
         m( 1, 0 ) = 3;
         m( 1, 1 ) = 4;
+        m( 1, 2 ) = 5;
+        m( 2, 0 ) = 6;
+        m( 2, 1 ) = 7;
+        m( 2, 2 ) = 8;
         return m;
     }
 
-    auto get_second_matrix_2d()
+    auto get_second_matrix_x3d()
     {
-        M2d m;
+        MXd m( 3, 3 );
         m.fill( 0 );
         m( 0, 0 ) = 1;
         m( 1, 1 ) = 1;
+        m( 2, 2 ) = 1;
         return m;
     }
 
     template < int i, int j >
     auto symmetric_derivative( const M2d& A, const M2d& B )
+    {
+        return compute_cofactor_directional_derivative< i, j >( A, B );
+    }
+
+    template < int i, int j >
+    auto symmetric_derivative_x2d( const MXd& A, const MXd& B )
     {
         return compute_cofactor_directional_derivative< i, j >( A, B );
     }
@@ -76,9 +128,84 @@ namespace
                compute_cofactor_directional_derivative< i, j >( B, A );
     }
 
+    template < int i, int j >
+    auto symmetric_derivative_x3d( const MXd& A, const MXd& B )
+    {
+        return compute_cofactor_directional_derivative< i, j >( A, B ) +
+               compute_cofactor_directional_derivative< i, j >( B, A );
+    }
+
 } // namespace
 
-TEST( CofactorTest_3d, D0 )
+TEST( DynamicCofactorTest_2d, D0 )
+{
+    const auto A = get_first_matrix_x2d();
+
+    auto value = compute_cofactor< 0, 0 >( A );
+    EXPECT_THAT( value, DoubleEq( A( 1, 1 ) ) );
+
+    value = compute_cofactor< 0, 1 >( A );
+    EXPECT_THAT( value, DoubleEq( -A( 1, 0 ) ) );
+
+    value = compute_cofactor< 1, 0 >( A );
+    EXPECT_THAT( value, DoubleEq( -A( 0, 1 ) ) );
+
+    value = compute_cofactor< 1, 1 >( A );
+    EXPECT_THAT( value, DoubleEq( A( 0, 0 ) ) );
+}
+
+TEST( DynamicCofactorTest_2d, D1 )
+{
+    const auto A = get_first_matrix_x2d();
+    const auto dA = get_second_matrix_x2d();
+    auto value = symmetric_derivative_x2d< 0, 0 >( A, dA );
+    EXPECT_THAT( value, DoubleEq( dA( 1, 1 ) ) );
+
+    value = symmetric_derivative_x2d< 0, 1 >( A, dA );
+    EXPECT_THAT( value, DoubleEq( -dA( 1, 0 ) ) );
+
+    value = symmetric_derivative_x2d< 1, 0 >( A, dA );
+    EXPECT_THAT( value, DoubleEq( -dA( 0, 1 ) ) );
+
+    value = symmetric_derivative_x2d< 1, 1 >( A, dA );
+    EXPECT_THAT( value, DoubleEq( dA( 0, 0 ) ) );
+}
+
+TEST( StaticCofactorTest_2d, D0 )
+{
+    const auto A = get_first_matrix_2d();
+
+    auto value = compute_cofactor< 0, 0 >( A );
+    EXPECT_THAT( value, DoubleEq( A( 1, 1 ) ) );
+
+    value = compute_cofactor< 0, 1 >( A );
+    EXPECT_THAT( value, DoubleEq( -A( 1, 0 ) ) );
+
+    value = compute_cofactor< 1, 0 >( A );
+    EXPECT_THAT( value, DoubleEq( -A( 0, 1 ) ) );
+
+    value = compute_cofactor< 1, 1 >( A );
+    EXPECT_THAT( value, DoubleEq( A( 0, 0 ) ) );
+}
+
+TEST( StaticCofactorTest_2d, D1 )
+{
+    const auto A = get_first_matrix_2d();
+    const auto dA = get_second_matrix_2d();
+    auto value = symmetric_derivative< 0, 0 >( A, dA );
+    EXPECT_THAT( value, DoubleEq( dA( 1, 1 ) ) );
+
+    value = symmetric_derivative< 0, 1 >( A, dA );
+    EXPECT_THAT( value, DoubleEq( -dA( 1, 0 ) ) );
+
+    value = symmetric_derivative< 1, 0 >( A, dA );
+    EXPECT_THAT( value, DoubleEq( -dA( 0, 1 ) ) );
+
+    value = symmetric_derivative< 1, 1 >( A, dA );
+    EXPECT_THAT( value, DoubleEq( dA( 0, 0 ) ) );
+}
+
+TEST( StaticCofactorTest_3d, D0 )
 {
     const auto A = get_first_matrix_3d();
 
@@ -110,7 +237,7 @@ TEST( CofactorTest_3d, D0 )
     EXPECT_THAT( value, DoubleEq( A( 0, 0 ) * A( 1, 1 ) - A( 1, 0 ) * A( 0, 1 ) ) );
 }
 
-TEST( CofactorTest_3d, D1 )
+TEST( StaticCofactorTest_3d, D1 )
 {
     const auto A = get_first_matrix_3d();
     const auto dA = get_second_matrix_3d();
@@ -142,57 +269,66 @@ TEST( CofactorTest_3d, D1 )
     EXPECT_THAT( value, DoubleEq( A( 0, 0 ) + A( 1, 1 ) ) );
 }
 
-TEST( CofactorTest_2d, D0 )
+TEST( DynamicCofactorTest_3d, D0 )
 {
-    const auto A = get_first_matrix_2d();
+    const auto A = get_first_matrix_x3d();
 
     auto value = compute_cofactor< 0, 0 >( A );
-    EXPECT_THAT( value, DoubleEq( A( 1, 1 ) ) );
+    EXPECT_THAT( value, DoubleEq( A( 1, 1 ) * A( 2, 2 ) - A( 1, 2 ) * A( 2, 1 ) ) );
 
     value = compute_cofactor< 0, 1 >( A );
-    EXPECT_THAT( value, DoubleEq( -A( 1, 0 ) ) );
+    EXPECT_THAT( value, DoubleEq( -A( 1, 0 ) * A( 2, 2 ) + A( 2, 0 ) * A( 1, 2 ) ) );
+
+    value = compute_cofactor< 0, 2 >( A );
+    EXPECT_THAT( value, DoubleEq( A( 1, 0 ) * A( 2, 1 ) - A( 2, 0 ) * A( 1, 1 ) ) );
 
     value = compute_cofactor< 1, 0 >( A );
-    EXPECT_THAT( value, DoubleEq( -A( 0, 1 ) ) );
+    EXPECT_THAT( value, DoubleEq( -A( 0, 1 ) * A( 2, 2 ) + A( 2, 1 ) * A( 0, 2 ) ) );
 
     value = compute_cofactor< 1, 1 >( A );
-    EXPECT_THAT( value, DoubleEq( A( 0, 0 ) ) );
+    EXPECT_THAT( value, DoubleEq( A( 0, 0 ) * A( 2, 2 ) - A( 2, 0 ) * A( 0, 2 ) ) );
+
+    value = compute_cofactor< 1, 2 >( A );
+    EXPECT_THAT( value, DoubleEq( -A( 0, 0 ) * A( 2, 1 ) + A( 2, 0 ) * A( 0, 1 ) ) );
+
+    value = compute_cofactor< 2, 0 >( A );
+    EXPECT_THAT( value, DoubleEq( A( 0, 1 ) * A( 1, 2 ) - A( 1, 1 ) * A( 0, 2 ) ) );
+
+    value = compute_cofactor< 2, 1 >( A );
+    EXPECT_THAT( value, DoubleEq( -A( 0, 0 ) * A( 1, 2 ) + A( 1, 0 ) * A( 0, 2 ) ) );
+
+    value = compute_cofactor< 2, 2 >( A );
+    EXPECT_THAT( value, DoubleEq( A( 0, 0 ) * A( 1, 1 ) - A( 1, 0 ) * A( 0, 1 ) ) );
 }
 
-TEST( CofactorTest_2d, D1 )
+TEST( DynamicCofactorTest_3d, D1 )
 {
-    const auto A = get_first_matrix_2d();
-    const auto dA = get_second_matrix_2d();
-    auto value = symmetric_derivative< 0, 0 >( A, dA );
-    EXPECT_THAT( value, DoubleEq( dA( 1, 1 ) ) );
+    const auto A = get_first_matrix_x3d();
+    const auto dA = get_second_matrix_x3d();
+    auto value = symmetric_derivative_x3d< 0, 0 >( A, dA );
+    EXPECT_THAT( value, DoubleEq( A( 1, 1 ) + A( 2, 2 ) ) );
 
-    value = symmetric_derivative< 0, 1 >( A, dA );
-    EXPECT_THAT( value, DoubleEq( -dA( 1, 0 ) ) );
+    value = symmetric_derivative_x3d< 0, 1 >( A, dA );
+    EXPECT_THAT( value, DoubleEq( -A( 1, 0 ) ) );
 
-    value = symmetric_derivative< 1, 0 >( A, dA );
-    EXPECT_THAT( value, DoubleEq( -dA( 0, 1 ) ) );
+    value = symmetric_derivative_x3d< 0, 2 >( A, dA );
+    EXPECT_THAT( value, DoubleEq( -A( 2, 0 ) ) );
 
-    value = symmetric_derivative< 1, 1 >( A, dA );
-    EXPECT_THAT( value, DoubleEq( dA( 0, 0 ) ) );
+    value = symmetric_derivative_x3d< 1, 0 >( A, dA );
+    EXPECT_THAT( value, DoubleEq( -A( 0, 1 ) ) );
 
-    // value = symmetric_derivative< 0, 2 >( A, dA );
-    // EXPECT_THAT( value, DoubleEq( -A( 2, 0 ) ) );
+    value = symmetric_derivative_x3d< 1, 1 >( A, dA );
+    EXPECT_THAT( value, DoubleEq( A( 0, 0 ) + A( 2, 2 ) ) );
 
-    // value = symmetric_derivative< 1, 0 >( A, dA );
-    // EXPECT_THAT( value, DoubleEq( -A( 0, 1 ) ) );
+    value = symmetric_derivative_x3d< 1, 2 >( A, dA );
+    EXPECT_THAT( value, DoubleEq( -A( 2, 1 ) ) );
 
-    // value = symmetric_derivative< 1, 1 >( A, dA );
-    // EXPECT_THAT( value, DoubleEq( A( 0, 0 ) + A( 2, 2 ) ) );
+    value = symmetric_derivative_x3d< 2, 0 >( A, dA );
+    EXPECT_THAT( value, DoubleEq( -A( 0, 2 ) ) );
 
-    // value = symmetric_derivative< 1, 2 >( A, dA );
-    // EXPECT_THAT( value, DoubleEq( -A( 2, 1 ) ) );
+    value = symmetric_derivative_x3d< 2, 1 >( A, dA );
+    EXPECT_THAT( value, DoubleEq( -A( 1, 2 ) ) );
 
-    // value = symmetric_derivative< 2, 0 >( A, dA );
-    // EXPECT_THAT( value, DoubleEq( -A( 0, 2 ) ) );
-
-    // value = symmetric_derivative< 2, 1 >( A, dA );
-    // EXPECT_THAT( value, DoubleEq( -A( 1, 2 ) ) );
-
-    // value = symmetric_derivative< 2, 2 >( A, dA );
-    // EXPECT_THAT( value, DoubleEq( A( 0, 0 ) + A( 1, 1 ) ) );
+    value = symmetric_derivative_x3d< 2, 2 >( A, dA );
+    EXPECT_THAT( value, DoubleEq( A( 0, 0 ) + A( 1, 1 ) ) );
 }
